9b71a64dacb94381f408f065bfac5e6deca8e60b
AisanSisani <aisan.sisani@gmail.com>
2021-06-26 09:55:43 +0300

    fixed cars.php a bit

--- cars.php
+++ cars.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Cars</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-	
     <!-- <link rel="stylesheet" href="css_files/expand_table_style.css"> -->
 	<!-- Navigation -->
 		<nav class="navbar navbar-expand-lg navbar-light static-top" style="background-color:#00C0CE;">
@@ -137,12 +136,17 @@
                             }
                         ?>                        
                         </table>
-                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            
 
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>    
-    <script>
+<script>
     $(document).ready(function(){
         $('.show_user_btn').on('click', function(){
             $tr = $(this).closest('tr');
