68d46ab9f1a655f34f8638ae2cdc156daf8ab7ca
Baraa-yusri <47057181+Baraa-yusri@users.noreply.github.com>
2021-06-27 13:32:24 +0300

    Add files via upload
    A file for the header of the taxi management pagae

---
+++ headertaxi.php
@@ -0,0 +1,15 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>metu ride share</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+		<div class="container" style="background-color:black; margin-top:20px; margin-bottom:20px;">
+			<h1 style="color:white; text-align:center;"> METU RIDE SHARE </h1>
+		</div>
+</head>
